c86c2782ef6ef09ed63d1cd35ce32b0273c26be8
Zackary Ayoun <ayoun_z@epita.fr>
2014-11-18 16:42:44 +0100

    Beep driver in progress...

--- beep_driver/beep.c
+++ beep_driver/beep.c
@@ -1,1 +1,42 @@
 #include "beep.h"
+
+int __init beep_init(void)
+{
+    int ret = platform_driver_register(&beep_funcs);
+    if (ret)
+        printk(KERN_ALERT "beep: error while registering platform driver.\n");
+    else
+        printk(KERN_INFO "beep: module initialized!\n");
+    return ret;
+}
+
+void __init beep_exit(void)
+{
+    platform_driver_unregister(&beep_funcs);
+    printk(KERN_INFO "beep: driver removed!\n");
+}
+
+static void make_beep(unsigned int freq)
+{
+    unsigned char beep_cur;
+    unsigned int pit_val;
+
+    pit_val = PIT_FREQUEN / freq;
+    outb(PIT_REGION2, PIT_SETTING);
+    outb(PIT_REGION1, (unsigned char)pit_val);
+    outb(PIT_REGION1, (unsigned char)(pit_val >> 8));
+
+    beep_cur = inb(BEEP_REGION);
+    if (beep_cur != (beep_cur | 3))
+        outb(BEEP_REGION, beep_cur | 3);
+}
+
+static void shutup_beep(void)
+{
+    unsigned char beep_cur = inb(BEEP_REGION) & 0xFC;
+    outb(BEEP_REGION, beep_cur);
+}
+
+static int treat_beep(struct input_dev *dev, unsigned int type, unsigned int code, int value)
+{
+}
--- beep_driver/beep.h
+++ beep_driver/beep.h
@@ -1,4 +1,57 @@
 #ifndef BEEP_H
 # define BEEP_H
 
+/* Including necessary headers. */
+# include <linux/platform_device.h>
+# include <linux/init.h>
+# include <linux/module.h>
+# include <linux/kernel.h> /* printk() */
+# include <asm/io.h>
+# include <linux/i8253.h>
+# include <linux/timex.h>
+# include <linux/input.h>
+
+# define BEEP_REGION 0x61
+# define PIT_REGION1 0x42
+# define PIT_REGION2 0x43
+# define PIT_SETTING 0xB6
+# define PIT_FREQUEN 1193180
+# define BEEP_FREQUE 1000
+
+/* Module informations */
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("Zackary Ayoun <zackaryayoun@gmail.com>");
+MODULE_DESCRIPTION("Little pc speaker beep driver.");
+
+/* Module function declarations */
+int __init beep_init(void);
+void __init beep_exit(void);
+int beep_suspend(struct device *dev);
+int __devinit beep_probe(struct platform_device *dev);
+int __devexit beep_remove(struct platform_device *dev);
+void beep_shutdown(struct platform_device *dev);
+
+/* Structures which declare driver operations */
+struct dev_pm_ops beep_ops =
+{
+    .suspend = beep_suspend,
+};
+
+struct platform_driver beep_funcs =
+{
+    .probe = beep_probe,
+    .remove = beep_remove,
+    .shutdown = beep_shutdown,
+    .driver =
+    {
+        .name = "beep",
+        .owner = THIS_MODULE,
+        .pm = &beep_ops,
+    },
+};
+
+/* Module init and exit declaration */
+module_init(beep_init);
+module_exit(beep_exit);
+
 #endif /* !BEEP_H */
